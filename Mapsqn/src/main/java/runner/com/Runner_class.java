0bdb9a7747d08391128159da59d276fecdc67d7d
bbhus <bbhus@DESKTOP-EKAV0QN>
2019-03-14 13:54:05 +0530

    finall

--- Mapsqn/src/main/java/runner/com/Runner_class.java
+++ Mapsqn/src/main/java/runner/com/Runner_class.java
@@ -3,6 +3,7 @@ package runner.com;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.CucumberOptions;
@@ -13,21 +14,5 @@ import cucumber.api.junit.Cucumber;
 
 public class Runner_class {
 
-	public void welcomePage() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\bbhusha3\\eclipse-workspace\\Mapsqn\\src\\main\\resources\\chromedriver.exe");
-
-		// Initialize browser
-		WebDriver driver = new ChromeDriver();
-		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		// Open facebook
-		driver.get("http://www.mapsynq.com/");
-
-		// Maximize browser
-
-		driver.manage().window().maximize();
-	}
-
+	
 }
--- Mapsqn/src/main/java/stepdefination/Opening_page_Step_Defination.java
+++ Mapsqn/src/main/java/stepdefination/Opening_page_Step_Defination.java
@@ -2,9 +2,9 @@ package stepdefination;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import runner.com.Runner_class;
+import step_method.Opening_page;
 
-public class Opening_page_Step_Defination extends Runner_class {
+public class Opening_page_Step_Defination extends Opening_page {
 
 	@Given("user is on Mapsynq welcome page")
 	public void user_is_on_Mapsynq_welcome_page() {
@@ -14,7 +14,48 @@ public class Opening_page_Step_Defination extends Runner_class {
 
 	@Then("Verify mapsynq title on welcome page")
 	public void verify_mapsynq_title_on_welcome_page() {
-		System.out.println();
+		VerifyTitle();
+	}
+
+	@Then("Verify Search bar on Mapsynq welcome page")
+	public void verify_Search_bar_on_Mapsynq_welcome_page() {
+		Searchbar();
+
+	}
+
+	@Then("Verify Sign in tab on welcome page")
+	public void verify_Sign_in_tab_on_welcome_page() {
+		signin();
+	}
+
+	@Then("verify Register tab on welcome page")
+	public void verify_Register_tab_on_welcome_page() {
+		registertab();
+	}
+
+	@Then("Verify Mobile App tab on welcome page")
+	public void verify_Mobile_App_tab_on_welcome_page() {
+		MobileApp();
+	}
+
+	@Then("Verify Galactio tab on welcome page")
+	public void verify_Galactio_tab_on_welcome_page() {
+		Galactiotab();
+	}
+
+	@Then("Verify SG GPS Navigation tab on welcome page")
+	public void verify_SG_GPS_Navigation_tab_on_welcome_page() {
+		SGGPSNavigation();
+	}
+
+	@Then("verify Calender tab on  welcome page")
+	public void verify_Calender_tab_on_welcome_page() {
+		Calendertab();
+	}
+
+	@Then("Verify legend tab on welcome page")
+	public void verify_legend_tab_on_welcome_page() {
+		legendtab();
 	}
 
 }
